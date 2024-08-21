1fd4ecf6b52f9f09ff3b00e84a450aa745b43d59
joseph-data <oluochjoseph00@gmail.com>
2024-08-21 15:18:32 +0200

    first part done!

--- functs.R
+++ functs.R
@@ -30,3 +30,48 @@ attributes(square)$srcref
 # custom attributes
 attr(square, "type") <- "polynomial"
 
+## multiple arguments and default values
+center1 <- function (x, fun = "mean", p = 0.5){
+  if(fun == "mean"){
+    x0 <- x - mean(x)
+  } else if(fun == "median"){
+    x0 <- x - median(x)
+  }else if(fun == "quantile"){
+    x0 <- x-quantile(x, probs = p)
+  }else{
+    stop("unrecognized function name")
+  }
+  return(x0)
+}
+
+y = (1:9)^2
+mean(y)
+center1(y, fun ="mean")
+center1(y, fun ="median")
+center1(y, fun = "quantile", p = 0.5)
+
+
+## calling a function within a function
+
+center3 <- function(x, fun = mean, ...){
+  x0 <- x - fun(x, ...)
+  return(x0)
+}
+
+center3(y, median)
+center3(y, quantile, probs = 0.5)
+
+## do.call on a string function
+#?do.call()
+do.call("mean", list(y))
+do.call("quantile", list(y, probs = 0.25))
+
+center4 <- function(x, fun = "mean", ...){
+  print(list(x, ...))
+  x0 <- x - do.call(fun, list(x, ...))
+  return(x0)
+}
+
+center4(y)
+center4(y, "median")
+center4(y, "quantile", probs = 0.25)
